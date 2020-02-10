0145cc59c2450c41cc76ba324d38bb3e01ff75b2
Waaade105 <artur.reinke@o2.pl>
2020-02-10 13:50:36 +0100

    the first real step towards a fully-functioning game application

--- Tic-Tac-Toe/task/src/tictactoe/Main.java
+++ Tic-Tac-Toe/task/src/tictactoe/Main.java
@@ -4,14 +4,35 @@ import java.util.Scanner;
 
 public class Main {
     /*
-    After printing the field, you need to find the state in which the game is at the moment. Possible states:
-    ' _ ' represents empty cell
-    "Game not finished" - when no side has a three in a row but the field has empty cells;
-    "Draw" - when no side has a three in a row and the field has no empty cells;
-    "X wins" - when the field has three X in a row;
-    "O wins" - when the field has three O in a row;
-    "Impossible" - when the field has three X in a row as well as three O in a row. Or the field has
-    a lot more X's that O's or vice versa (if the difference is 2 or more, should be 1 or 0).
+    IMPLEMENT HUMAN MOVES
+
+    Suppose the bottom left cell has the coordinates (1, 1) and the top right cell has the coordinates (3, 3) like in this table:
+
+    (1, 3) (2, 3) (3, 3)
+    (1, 2) (2, 2) (3, 2)
+    (1, 1) (2, 1) (3, 1)
+
+    The program should work in the following way:
+
+    Get the 3x3 field from the input as in the previous stages,
+    Output this 3x3 field with cells before the user's move,
+    Then ask the user about his next move,
+    Then the user should input 2 numbers that represent the cell on which user wants to make his X or O.
+    (9 symbols representing the field would be on the first line and these 2 numbers would be on the second line of the user input),
+    Then output the table including the user's most recent move.
+    Do not delete code that checks for table state; it will be useful in the future.
+
+    Note that in this stage user moves as X, not O. Keep in mind that the first coordinate goes from left to right
+    and the second coordinate goes from bottom to top. Also, notice that coordinates start with 1 and can be 1, 2 or 3.
+
+    But what if the user enters incorrect coordinates? The user could enter symbols instead of numbers or enter coordinates
+    representing occupied cells. You need to prevent all of that by checking a user's input and catching possible exceptions.
+
+    The program should also check user input. If the user input is unsuitable, the program should ask him to enter coordinates again.
+
+    So, you need to output a field from the first line of the input and then ask the user to enter a move.
+    Keep asking until the user enters coordinate that represents an empty cell on the field and after that output
+    the field with that move. You should output the field only 2 times - before the move and after a legal move.
      */
 
     public static void main(String[] args) {
